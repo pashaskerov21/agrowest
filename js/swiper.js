e190bd4fd8ad2acc05fcef7ef190fd27420f9483
pasha <askerovpasha21@gmail.com>
2024-01-07 17:17:08 +0400

    products section completed

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const bannerSwiper = new Swiper('.banner-swiper', {
-        loop: true,
-        // autoplay: {
-        //     delay: 5000,
-        //     disableOnInteraction: false,
-        // },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            
-        },
-    });
+    $(document).ready(function(){
+        const bannerSwiper = new Swiper('.banner-swiper', {
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 3,
+            },
+        });
+        const productSwiper = new Swiper('.products-swiper', {
+            slidesPerView: 1,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 6,
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    spaceBetween: 20,
+                },
+                992: {
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                1200: {
+                    slidesPerView: 4,
+                    slidesPerGroup: 4,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+            }
+        });
+    })
 });
