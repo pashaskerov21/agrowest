a5f8956cd07bacbfa7e64a3c93f2d0380db73e8c
pasha <askerovpasha21@gmail.com>
2024-01-07 14:04:39 +0400

    header completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+    $(document).ready(function () {
+        $('header .menu__button').click(function () {
+            $('.menu__wrapper').addClass('active');
+            $('body').css('overflow', 'hidden');
+        });
+        $('.menu__wrapper .close__button').click(function () {
+            $('.menu__wrapper').removeClass('active');
+            $('body').css('overflow', 'visible');
+        });
+    })
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+    const bannerSwiper = new Swiper('.banner-swiper', {
+        loop: true,
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            
+        },
+    });
+});
