ec07701c8a64905c6ad604a92283aa347ff3422e
pasha <askerovpasha21@gmail.com>
2024-01-07 17:51:16 +0400

    blogs section completed

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $(document).ready(function(){
+    $(document).ready(function () {
         const bannerSwiper = new Swiper('.banner-swiper', {
             loop: true,
             autoplay: {
@@ -47,5 +47,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
             }
         });
+        const blogSwiper = new Swiper('.blogs-swiper', {
+            slidesPerView: 1,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 3,
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    spaceBetween: 20,
+                },
+                992: {
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 20,
+                },
+            }
+        })
     })
 });
